26bb0ae8f15e5a5dfb4595d0fb57021e5706ee14
Yuta Takahashi <y.highbridge04@gmail.com>
2024-04-13 23:57:01 +0900

    add actix-web template and version return endpoint

--- public-api/src/main.rs
+++ public-api/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::fs;
+use toml::Value;
+
+/// Return anemos public api's Version
+async fn index() -> impl Responder {
+    let contents = fs::read_to_string("Cargo.toml").expect("Something went wrong reading the file");
+    let parsed_value = contents.parse::<Value>().unwrap();
+    let package = parsed_value.get("package").unwrap().get("version").unwrap().as_str().unwrap();
+
+    HttpResponse::Ok().body(package.to_string())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/v1/", web::get().to(index))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
