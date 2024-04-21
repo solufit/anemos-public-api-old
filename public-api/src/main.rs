b9f665cc7e42c097308287344210be30c679d0a0
Yuta Takahashi <y.highbridge04@gmail.com>
2024-04-21 17:39:38 +0900

    Add log feature
    This commit includes the addition of the log feature. The following files have been modified:
    - .vscode/launch.json
    - public-api/Cargo.lock
    - public-api/Cargo.toml
    - public-api/src/main.rs

--- public-api/src/main.rs
+++ public-api/src/main.rs
@@ -18,8 +18,15 @@ async fn index() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    env_logger::init();
+    log::info!("Starting Anemos Public API Server at port 8080");
+    log::debug!("Debugging mode enabled");
+    
+
     HttpServer::new(|| {
         App::new()
+            .wrap(actix_web::middleware::Logger::default())
             .configure(config)
     })
     .bind("0.0.0.0:8080")?
