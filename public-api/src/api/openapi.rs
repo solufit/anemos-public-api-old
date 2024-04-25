4b3400ffeac06208888e63a236bb1af089fa1778
Yuta Takahashi <y.highbridge04@gmail.com>
2024-04-25 23:14:57 +0900

    add description to api docs

--- public-api/src/api/openapi.rs
+++ public-api/src/api/openapi.rs
@@ -1,11 +1,12 @@
 use utoipa::OpenApi; 
 
+
 #[derive(OpenApi)]
 #[openapi(
     info(
-        title = "Utoipa API",
-        version = "0.1.0",
-        description = "API for"
+        title = "Anemos API",
+        description = "Anemos API made by solufit. For detail, please visit the web site. https://anemos.solufit.net/api",
+        version = "0.1.0"
     ),
     paths(
         super::versions::versions,
