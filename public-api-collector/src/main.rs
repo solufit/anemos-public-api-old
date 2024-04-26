9e9d0aa887f1f02aaff3e79a15ee6d1e025abd8c
Yuta Takahashi <y.highbridge04@gmail.com>
2024-04-26 21:12:44 +0900

    "Create a template for interval execution."

--- public-api-collector/src/main.rs
+++ public-api-collector/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::time::{interval, Duration};
+
+async fn abc() {
+    // Your code here
+    log::info!("Run every 10s");
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let mut interval = interval(Duration::from_secs(10));
+    log::info!("Starting Data Collector for Anemos Public API Version: {}", env!("CARGO_PKG_VERSION"));
+    loop {
+        interval.tick().await;
+        abc().await;
+    }
 }
