8fa4bf9e44c7060c1b17d4500bcecb3aa0281859
Yuta Takahashi <y.highbridge04@gmail.com>
2024-05-01 20:14:07 +0900

    Add timing information to data collection

--- public-api-collector/src/main.rs
+++ public-api-collector/src/main.rs
@@ -5,7 +5,7 @@ use std::pin::Pin;
 
 use std::vec;
 
-use tokio::time::{interval, Duration};
+use tokio::time::{interval, Duration, Instant};
 
 #[tokio::main]
 async fn main() {
@@ -16,10 +16,16 @@ async fn main() {
 
     log::info!("Starting Data Collector for Anemos Public API Version: {}", env!("CARGO_PKG_VERSION"));
     loop {    
+        log::info!("Starting Data Collection every 10 Seconds");
+        let instant = Instant::now();
+
         let futures: Vec<Pin<Box<dyn Future<Output = ()> + Send>>> = vec![
         Box::pin(crate::earthquake::collect_earthquake::earthquake_operator()),
     ];
-        interval.tick().await;
         futures::future::join_all(futures).await;
+
+        log::info!("Data Collection Finished in {} milli seconds", instant.elapsed().as_millis());
+
+        interval.tick().await;
     }
 }
