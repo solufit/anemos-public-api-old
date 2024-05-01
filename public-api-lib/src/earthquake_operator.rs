72c57156b10b8fda0cef5df654daa4f29a7ecd68
Yuta Takahashi <y.highbridge04@gmail.com>
2024-05-01 22:40:46 +0900

    Add earthquake_operator module and test

--- public-api-lib/src/earthquake_operator.rs
+++ public-api-lib/src/earthquake_operator.rs
@@ -19,4 +19,31 @@ fn eventid_extractor(earthquake: &EarthQuake) -> String {
 
 
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_eventid_extractor() {
+        let earthquake = EarthQuake {
+            Eventid: 12345.to_string(),
+            id: "id".to_string(),
+            EditorialOffice: "EditorialOffice".to_string(),
+            PublishingOffice: "PublishingOffice".to_string(),
+            Category: "Category".to_string(),
+            Datetime: "Datetime".to_string(),
+            Headline: "Headline".to_string(),
+            Hypocenter: "Hypocenter".to_string(),
+            RegionCode: "RegionCode".to_string(),
+            MaxInt: "MaxInt".to_string(),
+            Magnitude: 1.0,
+            Intensity: None,
+
+            
+        };
+
+        let event_id = eventid_extractor(&earthquake);
+        assert_eq!(event_id, "12345");
+    }
+}
 
--- public-api-lib/src/lib.rs
+++ public-api-lib/src/lib.rs
@@ -2,3 +2,4 @@
 pub mod scheme;
 pub mod get_env;
 pub mod redis;
+pub mod earthquake_operator;
