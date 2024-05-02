0adbaedd7ef25a939edd6aa3b357ca971d195700
Yuta Takahashi <y.highbridge04@gmail.com>
2024-05-02 19:00:36 +0900

    Add async function to retrieve data from Redis

--- public-api-lib/src/earthquake_operator.rs
+++ public-api-lib/src/earthquake_operator.rs
@@ -1,6 +1,7 @@
 use std::fmt::format;
 use std::collections::HashMap;
 use anyhow::{Ok, Result, Error};
+use futures::{task::waker, Future};
 use log::{debug, info};
 use crate::scheme::earthquake::EarthQuake;
 use crate::redis::redisOperation;
@@ -21,11 +22,18 @@ fn eventid_extractor(earthquake: &EarthQuake) -> String {
 }
 
 
+async fn get_from_redis(event_id: String) -> Result<String, Error> {
+    let mut redis_op = redisOperation::new().await?;
+
+    let result = Cmd::get(format!("earthquake-detail-{}", event_id))
+        .query_async(&mut redis_op.multiplexed_connection).await.unwrap_or("".to_string());
+    Ok(result)
+}
+
 async fn earthquake_data_submitter(earthquake: &Vec<EarthQuake>) -> Result<(), Error> {
     // Submit the data to the API
 
     let mut redis_op = redisOperation::new().await?;
-    let mut pipe = Pipeline::new();
     let mut event_id_list: Vec<String> = Vec::new();
     let mut hash_earthquake: HashMap<String, &EarthQuake> = HashMap::new();
 
@@ -33,21 +41,17 @@ async fn earthquake_data_submitter(earthquake: &Vec<EarthQuake>) -> Result<(), E
 
     // add to hashmap to check dubplicate
     // check if the event_id is already in the database
-    let _ = earthquake.iter().map(|e| {
+
+    let cmds: Vec<_> = earthquake.iter().map(|e| {
         let event_id = eventid_extractor(e);
         event_id_list.push(event_id.clone());
-        pipe.get(format!("earthquake-detail-{}", event_id));
-        hash_earthquake.insert(event_id, e);
-    });
-    
+        hash_earthquake.insert(event_id.clone(), e);
+        get_from_redis(event_id)
 
-    // get cached data 
+    }).collect();
 
-    let command_num = pipe.cmd_iter().count();
-
-    let result = redis_op.multiplexed_connection
-                .send_packed_commands(&pipe, 0, command_num).await;
-
+    let results = futures::future::join_all(cmds).await
+            .into_iter().collect::<Result<Vec<String>>>()?;
 
 
     Ok(())
