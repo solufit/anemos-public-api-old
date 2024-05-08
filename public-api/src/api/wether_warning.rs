f6b12ef57ee2e37fda89386f4821d60fe4277903
Yuta Takahashi <y.highbridge04@gmail.com>
2024-05-08 18:38:33 +0900

    Add weather warning API endpoint

--- public-api/src/api/wether_warning.rs
+++ public-api/src/api/wether_warning.rs
@@ -6,6 +6,8 @@ use actix_web::{
 };
 use serde::{Deserialize, Serialize};
 use public_api_lib::scheme::weather_warning::WeatherWarning;
+use log::{debug, info};
+use once_cell::sync::Lazy;
 
 
 
@@ -17,14 +19,16 @@ use public_api_lib::scheme::weather_warning::WeatherWarning;
 )]
 #[get("/v1/weather_warning/get/{num}")]
 pub async fn get_weather_warning(num: web::Path<u32>) -> impl Responder {
+    let max_num = num.into_inner();
     
     static BASE_URL: Lazy<std::string::String> = Lazy::new(|| {
-        format!("{}/get/weatherwarning?max={}", *public_api_lib::get_env::API_URL, num)
+        format!("{}/get/weatherwarning?max=", *public_api_lib::get_env::API_URL)
     });
+    let base_url_system = BASE_URL.to_string() + &max_num.to_string();
 
-    log::info!("Collecting Weather Warning Data from: {}", &BASE_URL.to_string());
+    log::info!("Collecting Weather Warning Data from: {}", &base_url_system);
 
-    let response = reqwest::get(BASE_URL.as_str())
+    let response = reqwest::get(base_url_system.as_str())
         .await.unwrap()
         .text()
         .await.unwrap();  
