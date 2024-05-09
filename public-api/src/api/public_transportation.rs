9195d82aa0721fba21c2afbc52cff1b499437988
Yuta Takahashi <y.highbridge04@gmail.com>
2024-05-09 16:24:56 +0900

    Update public transportation API endpoints

--- public-api/src/api/public_transportation.rs
+++ public-api/src/api/public_transportation.rs
@@ -17,7 +17,7 @@ use utoipa::openapi::{response, security::Http};
         (status = 200, description = "Public Transportation Area", body =  String)
     )
 )]
-#[get("/v1/public-transportation/area")]
+#[get("/v1/public-transportation/get-area")]
 pub async fn get_public_transportation_area() -> impl Responder {
     static BASE_URL: Lazy<std::string::String> = Lazy::new(|| {
         format!("{}/config/pti/area", *public_api_lib::get_env::API_URL)
@@ -55,12 +55,51 @@ pub async fn get_public_transportation_area() -> impl Responder {
         (status = 200, description = "Public Transportation line")
     )
 )]
-#[get("/v1/public-transportation/line/{area}")]
-pub async fn get_public_transportation_line(area: web::Path<String>) -> impl Responder {
+#[get("/v1/public-transportation/get-line/{area}")]
+pub async fn get_public_transportation_area_to_line(area: web::Path<String>) -> impl Responder {
+    static BASE_URL: Lazy<std::string::String> = Lazy::new(|| {
+        format!("{}/config/pti/area", *public_api_lib::get_env::API_URL)
+    });
+    let base_url = format!("{}?area={}", *BASE_URL, area);
+
+    match reqwest::get(base_url).await {
+        Ok(response) => {
+
+            match response.text().await {
+                Ok(text) => {
+                    debug!("Recieved Response: {}", text);
+
+                    return HttpResponse::Ok().body(text)
+                }
+                Err(e) => {
+                    log::error!("ERR! {}", e);
+                    return HttpResponse::InternalServerError().body("ERR!")
+                }
+            }
+            
+        }
+        Err(e) => {
+            log::error!("ERR! {}",  e);
+            return HttpResponse::InternalServerError().body("ERR!")
+        }
+    }
+
+
+}
+
+#[allow(clippy::needless_return)]
+#[utoipa::path(
+    get,
+    responses(
+        (status = 200, description = "Public Transportation line")
+    )
+)]
+#[get("/v1/public-transportation/get-line/{line}")]
+pub async fn get_public_transportation_line(line: web::Path<String>) -> impl Responder {
     static BASE_URL: Lazy<std::string::String> = Lazy::new(|| {
         format!("{}/config/pti/line", *public_api_lib::get_env::API_URL)
     });
-    let base_url = format!("{}?area={}", *BASE_URL, area);
+    let base_url = format!("{}?line={}", *BASE_URL, line);
 
     match reqwest::get(base_url).await {
         Ok(response) => {
